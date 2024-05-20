1e493110a836e498191c78a0bea343da15561d56
Diwa Oliq <diwaoliq@gmail.com>
2024-05-20 14:29:42 +0700

    register done

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 from typing import Final
 from telegram import Update, ReplyKeyboardMarkup, ReplyKeyboardRemove
 from telegram.ext import Application, CommandHandler, MessageHandler, ConversationHandler, filters, ContextTypes
@@ -8,11 +7,11 @@ TOKEN: Final = '6862559344:AAHYZG_2VSnYsdyArs62WogXHRLLQxdBMkw'
 BOT_USERNAME: Final = '@yoursafetybelt_teddyBot'
 
 # Define states for the conversation
-NAME, CHILD_NAME, RELATIONSHIP, GENDER = range(4)
+NAME, CHILD_NAME, RELATIONSHIP, ID_SABUK, GENDER = range(5)
 
-## Commands
+# Commands
 async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text('Hello! Selamat datang di layanan kami ' + emoji.emojize(':grinning_face_with_big_eyes:'))
+    await update.message.reply_text('Halo! Selamat datang di layanan kami ' + emoji.emojize(':grinning_face_with_big_eyes:'))
 
 async def register_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text('Nama Anda?\n\n Jika ingin membatalkan pendaftaran silahkan ketik /cancel')
@@ -22,7 +21,7 @@ async def name_received(update: Update, context: ContextTypes.DEFAULT_TYPE):
     user_name = update.message.text.strip()
     if not user_name.replace(" ", "").isalpha():
         await update.message.reply_text('Nama tidak boleh mengandung simbol atau angka. Silakan masukkan nama Anda tanpa simbol atau angka.')
-        return NAME  # Mengulangi langkah untuk meminta nama lagi
+        return NAME
     else:
         context.user_data['name'] = user_name
         await update.message.reply_text('Nama buah hati Anda?')
@@ -32,7 +31,7 @@ async def child_name_received(update: Update, context: ContextTypes.DEFAULT_TYPE
     child_name = update.message.text.strip()
     if not child_name.replace(" ", "").isalpha():
         await update.message.reply_text('Nama buah hati tidak boleh mengandung simbol atau angka. Silakan masukkan nama buah hati Anda tanpa simbol atau angka.')
-        return CHILD_NAME  # Mengulangi langkah untuk meminta nama buah hati lagi
+        return CHILD_NAME
     else:
         context.user_data['child_name'] = child_name
         reply_keyboard = [['Orang Tua', 'Guru', 'Kerabat']]
@@ -45,6 +44,12 @@ async def child_name_received(update: Update, context: ContextTypes.DEFAULT_TYPE
 async def relationship_received(update: Update, context: ContextTypes.DEFAULT_TYPE):
     relationship = update.message.text
     context.user_data['relationship'] = relationship
+    await update.message.reply_text('ID sabuk Anda?')
+    return ID_SABUK
+
+async def id_sabuk_received(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    id_sabuk = update.message.text.strip()
+    context.user_data['id_sabuk'] = id_sabuk
     reply_keyboard = [['Laki-laki', 'Perempuan']]
     await update.message.reply_text(
         "Jenis kelamin Anda?",
@@ -58,9 +63,10 @@ async def gender_received(update: Update, context: ContextTypes.DEFAULT_TYPE):
     user_data = context.user_data
 
     biodata = (f"Terima kasih telah mendaftar!{emoji.emojize(':grinning_face_with_big_eyes:') }\n\n" 
-               f"---Berikut Biodata Anda---\n"
+               f"--- Berikut Biodata Anda ---\n"
                f"Nama Anda: {user_data['name']}\n"
                f"Nama buah hati: {user_data['child_name']}\n"
+               f"ID Sabuk: {user_data['id_sabuk']}\n"
                f"Hubungan: {user_data['relationship']}\n"
                f"Jenis kelamin: {user_data['gender']}")
 
@@ -90,22 +96,23 @@ async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text('Apa yang dapat kami bantu?')
     await update.message.reply_text('Jika bantuan ini kurang membantu anda dapat menghubungi kontak berikut: (No.Telp) ')
 
-if __name__ == '__main__':
+if _name_ == '_main_':
     app = Application.builder().token(TOKEN).build()
 
-    ## ConversationHandler for registration
+    # ConversationHandler for registration
     conv_handler = ConversationHandler(
         entry_points=[CommandHandler('register', register_command)],
         states={
             NAME: [MessageHandler(filters.TEXT & ~filters.COMMAND, name_received)],
             CHILD_NAME: [MessageHandler(filters.TEXT & ~filters.COMMAND, child_name_received)],
+            ID_SABUK: [MessageHandler(filters.TEXT & ~filters.COMMAND, id_sabuk_received)],
             RELATIONSHIP: [MessageHandler(filters.TEXT & ~filters.COMMAND, relationship_received)],
             GENDER: [MessageHandler(filters.TEXT & ~filters.COMMAND, gender_received)]
         },
         fallbacks=[CommandHandler('cancel', cancel)]
     )
 
-    ## Add handlers to the application
+    # Add handlers to the application
     app.add_handler(CommandHandler('start', start_command))
     app.add_handler(CommandHandler('connect', connect_command))
     app.add_handler(CommandHandler('status', status_command))
@@ -115,6 +122,6 @@ if __name__ == '__main__':
     app.add_handler(CommandHandler('help', help_command))
     app.add_handler(conv_handler)
 
-    ## Polls the bot
+    # Polls the bot
     print('Polling...')
-    app.run_polling(poll_interval=3)
+    app.run_polling(poll_interval=2)
